07919f0f600445347e75137a7a482b4bc01769e9
Adrian Sims <adriansims62781@gmail.com>
2019-01-05 12:58:51 +0100

    Add project files.

---
+++ ConvertSubs/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertSubs
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {   
+            try
+            {
+                if (args.Length == 0)
+                {
+                    throw new Exception("You must supply one or more subtitle files to convert to UTF-8");
+                }
+
+                foreach (var filename in args)
+                {
+                    var outputfile = filename + ".utf8";
+                    var content = File.ReadAllText(filename, Encoding.Default);
+                    File.WriteAllText(filename, Encoding.UTF8);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                var codeBase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+                var progname = Path.GetFileNameWithoutExtension(codeBase);
+                Console.Error.WriteLine(progname + ": Error: " + ex.Message);
+            }
+
+        }
+    }
+}
---
+++ gitmehappy.cmd
@@ -0,0 +1,8 @@
+rem echo off
+git init
+git add ConvertSubs/Program.cs
+git add ConvertSubs/App.config
+git add ConvertSubs/Properties/AssemblyInfo.cs
+git add ConvertSubs/ConvertSubs.csproj
+git add ConvertSubs.sln
+git add .gitignore
